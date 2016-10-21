ba2d5e7347c5644c929ae54254889ca70cd572d7
resoft <wayne.chen0507@gmail.com>
2016-10-21 21:12:54 +0800

    第一個紅燈

---
+++ Day2-ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_ShoppingCart
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+
+        public object Checkout(List<Book> orders)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Book
+    {
+        public int Quantity { get; set; }
+   
+    }
+}
---
+++ Day2-ShoppingCartTests/ShoppingCartTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day2_ShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_ShoppingCart.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void ShoppingCart_Of_1_Potter_Book_Should_Be_100()
+        {
+            var expected = 100;
+            var orders = new List<Book>()
+            {
+                  new Book {Quantity = 1 }
+            };
+
+            var shoppingCart = new ShoppingCart();
+
+            var actual = shoppingCart.Checkout(orders);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+   
+}
+
+
+
+
